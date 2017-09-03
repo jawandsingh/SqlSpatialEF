d2941f3497c49aa8c3dad2b6167587a0842e403d
hackdecode- Jawand Singh <jawand.virk@gmail.com>
2017-09-03 16:05:51 +0530

    created a helper method to Create a Point for SQL Spatial

--- Hackdecode.SQLSpatial/Program.cs
+++ Hackdecode.SQLSpatial/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Spatial;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace Hackdecode.SQLSpatial
     {
         static void Main(string[] args)
         {
+        }
+
+        /// <summary>
+        /// Create Point using a helper method 
+        /// </summary>
+        /// <param name="latitude"></param>
+        /// <param name="longitude"></param>
+        /// <returns></returns>
+        public static DbGeography CreatePoint(double latitude, double longitude)
+        {
+            // 4326 is most common used coordinate system used in Maps
+            return DbGeography.FromText($"POINT({latitude} {longitude})", 4326);
         }
     }
 }
